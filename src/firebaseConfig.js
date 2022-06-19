34e91a481b23f432afac1755930e1d4426def2b8
arnobchakma <arnobchakma05@gmail.com>
2022-06-18 20:42:26 -0600

    nothing

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBv_GpNyr7aei0uldmjWFmR8OV1xDmvuIg",
+    authDomain: "crud-redux-b1b90.firebaseapp.com",
+    projectId: "crud-redux-b1b90",
+    storageBucket: "crud-redux-b1b90.appspot.com",
+    messagingSenderId: "484050193637",
+    appId: "1:484050193637:web:690c508093b6b2dc5f580c"
+};
+
+export default firebaseConfig;

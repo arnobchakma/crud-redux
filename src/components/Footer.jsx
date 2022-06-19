88b766e63efd42297a700245c0cb7e1425227377
arnobchakma <arnobchakma05@gmail.com>
2022-06-19 12:19:35 -0600

    spelling mistake

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="text-center text-indigo-600">
-            <p>Copyright {(new Date()).getFullYear()} | Privacy Policy | All Rights Reserved Mercent</p>
+            <p>Copyright {(new Date()).getFullYear()} | Privacy Policy | All Rights Reserved Merchant</p>
         </div>
     );
 };
